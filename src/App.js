186d65c1a9fa09efa961a31946afbd19d6ede0bd
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-09-21 08:53:26 -0300

    añade importacion de componentes, y modificacion de estado

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import React, {useState} from 'react';
 import './App.css';
-import Burger from './components/Burger/Burger';
-import Cart from './components/Cart/Cart';
 
+import { ItemContainer } from './components/Container/ItemContainer';
+import { Header } from './components/Header/Header';
+import { Footer } from "./components/Footer/Footer"
 function App() {
   
-  const [burgers, setBurgers] = useState([
-    {id:1, nombre:"Burger completa", precio:250},
-    {id:2, nombre:"Burger con Cheddar", precio:180},
-    {id:3, nombre:"Burger con Jamón y Queso", precio:150},
-    {id:4, nombre:"Burger con Bacon", precio: 200},
-    {id:5, nombre:"Burger de pollo completa", precio: 240},
-  ])
+  
   const [cart, setCart] = useState([])
   return (
     <>
-    <h3>Burgers</h3>
-    {burgers.map((burger) =>(
-      <Burger
-      key={burger.id}
-      burger={burger}
-      cart={cart}//estado carrito
-      setCart={setCart}//modificador de estado carrito
-      burgers={burgers}//listado de hamburguesas
-      />
-    ))}
-    <Cart
-    cart={cart}
-    setCart={setCart}
-    />
+    <section className="container">
+        <Header cart={cart} setCart={setCart}/>
+        <ItemContainer cart={cart} setCart={setCart}/>
+        <Footer/>
+    </section>
     </>
   );
 }
